6a416caa6f09756fbc8e752c4dd32cdb51455021
iamrivera <a3m3rivera@gmail.com>
2021-09-25 23:20:32 -0700

    About page is properly setup and exporting.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import About from "./components/about";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <About />
+      </div>
+    );
+  }
 }
 
 export default App;
