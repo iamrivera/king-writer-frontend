6779d8b6304285893b672a715966a036f4f05cc6
iamrivera <a3m3rivera@gmail.com>
2021-09-25 23:21:37 -0700

    Home page properly setup and exporting.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React, { Component } from "react";
 import About from "./components/about";
+import Home from "./components/home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <About />
+        <Home />
       </div>
     );
   }
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="container">This is my home page!</div>;
+};
+
+export default Home;
