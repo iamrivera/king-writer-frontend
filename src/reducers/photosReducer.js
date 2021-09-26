18203a0c7ce7392d3f2a87f7fe939463c3a688c8
iamrivera <a3m3rivera@gmail.com>
2021-09-25 22:31:11 -0700

    Created the intial photosReducer. Staging action for photos next.

---
+++ src/reducers/photosReducer.js
@@ -0,0 +1,12 @@
+
+
+export default function photosReducer(state = [], action) {
+    switch (action.type) {
+
+    case "GET_PHOTOS_SUCCESS":
+        return [...state, action.payload]
+
+    default:
+        return state
+    }
+}
