32a916c5f3ebca8d838fc0d83c2d0afa502de0e2
iamrivera <a3m3rivera@gmail.com>
2021-09-26 00:42:05 -0700

    Mapped the state to props. Mapped the getPosts action to props. Begin building posts component.

--- src/actions/getPosts.js
+++ src/actions/getPosts.js
@@ -1,4 +1,4 @@
-export function getPosts() {
+export default function getPosts() {
   return (dispatch) => {
     fetch("http://127.0.0.1:3000/posts")
       .then((response) => response.json())
--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import rootReducer from '../reducers/rootReducer';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import getPosts from "../actions/getPosts";
 
 class PostsContainer extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
 
-    componentDidMount() {
-        this.props.getPosts()
-    }
-
-    render() {
-        return (
-            <h1>Hello</h1>
-        )
-    }
-
+  render() {
+    return <h1>Hello</h1>;
+  }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
 
-
-    const mapStateToProps = state => {
-        return {
-            posts: state.posts
-        }
-    }
-
-    const mapDispatchToProps = (dispatch) => {
-        return {
-          getPosts: () => dispatch(getPosts()),
-        };
-      };
-      
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPosts: () => dispatch(getPosts()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
