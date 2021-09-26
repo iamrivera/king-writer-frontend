5be8935518e6e07c8c870a1e65d585b352b91329
iamrivera <a3m3rivera@gmail.com>
2021-09-25 21:12:33 -0700

    Imported BrowserRouter as Router. Nested within Provider. Wrapped App.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
@@ -15,7 +16,9 @@ const store = createStore(composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
