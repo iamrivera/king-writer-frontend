807d3819b2568aab9bb253ecdd0284f687588de7
iamrivera <a3m3rivera@gmail.com>
2021-09-26 01:48:48 -0700

    Created the PostForm basic layout. Adding functionality next.

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {Form, Button, Col, Row } from 'react-bootstrap';
 
 class PostForm extends Component {
-    
-    render() {
-        return (
-            <div>
-                <h1>Hello</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container mt-3">
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalTitle">
+            <Form.Label column sm={2}>
+              Title
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="title" placeholder="Give Your Post a Fun Title" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalText"
+          >
+            <Form.Label column sm={2}>
+              Content
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="textarea" placeholder="Write Your Story Here" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Publish Story</Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default PostForm
+export default PostForm;
