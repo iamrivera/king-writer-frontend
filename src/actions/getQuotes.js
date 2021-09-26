36c075b6a412dab327035751a2ea85ca6ed18e04
iamrivera <a3m3rivera@gmail.com>
2021-09-25 22:59:38 -0700

    Cleaned out APP boiler plate. Thinking about how to best test redux state is working when data fetched.

--- src/actions/getQuotes.js
+++ src/actions/getQuotes.js
@@ -1,13 +1,12 @@
 export function getQuotes() {
-    return (dispatch) => {
-      fetch("http://127.0.0.1:3000/quotes")
-        .then((response) => response.json())
-        .then((quotes) =>
-          dispatch({
-            type: "GET_QUOTES_REQUEST",
-            payload: quotes,
-          })
-        );
-    };
-  }
-  
+  return (dispatch) => {
+    fetch("http://127.0.0.1:3000/quotes")
+      .then((response) => response.json())
+      .then((quotes) =>
+        dispatch({
+          type: "GET_QUOTES_REQUEST",
+          payload: quotes,
+        })
+      );
+  };
+}
--- src/reducers/quotesReducer.js
+++ src/reducers/quotesReducer.js
@@ -1,10 +1,9 @@
 export default function quotesReducer(state = [], action) {
-    switch (action.type) {
-      case "GET_QUOTES_REQUEST":
-        return [...state, action.payload];
-  
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case "GET_QUOTES_REQUEST":
+      return [...state, action.payload];
+
+    default:
+      return state;
   }
-  
+}
