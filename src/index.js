34722c454ec0b6504894076fbc91763d857377de
iamrivera <a3m3rivera@gmail.com>
2021-09-25 21:08:37 -0700

    Imported applyMiddleware and thunk.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react";
-import { createStore, compose } from "redux";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import "./bootstrap.min2.css";
 import "./index.css";
@@ -13,8 +14,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore();
 
 ReactDOM.render(
- <Provider store={store}>
-   <App />
- </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
