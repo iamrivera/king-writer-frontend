b9fc136155fc1827d5ffe90b7ce62e56b535bb5b
iamrivera <a3m3rivera@gmail.com>
2021-09-25 20:58:52 -0700

    Imported createStore from Redux. Created store const.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
 import "./bootstrap.min2.css";
 import "./index.css";
+
 import App from "./App";
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
