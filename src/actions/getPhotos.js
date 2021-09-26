63c163be1f7e6efe61d22189340a7f973e79362b
iamrivera <a3m3rivera@gmail.com>
2021-09-25 22:38:05 -0700

    Created the getPhotos action

---
+++ src/actions/getPhotos.js
@@ -0,0 +1,12 @@
+export function getPhotos() {
+  return (dispatch) => {
+    fetch("http://127.0.0.1:3000/photos")
+      .then((response) => response.json())
+      .then((photos) =>
+        dispatch({
+          type: "GET_PHOTOS_SUCCESS",
+          payload: photos,
+        })
+      );
+  };
+}
