3c9c6ad9abe269041809921a487fd1ab7eff5fbb
iamrivera <a3m3rivera@gmail.com>
2021-09-25 22:48:16 -0700

    Created quotes action

---
+++ src/actions/getQuotes.js
@@ -0,0 +1,13 @@
+export function getQuotes() {
+    return (dispatch) => {
+      fetch("http://127.0.0.1:3000/quotes")
+        .then((response) => response.json())
+        .then((quotes) =>
+          dispatch({
+            type: "GET_QUOTES_REQUEST",
+            payload: quotes,
+          })
+        );
+    };
+  }
+  
