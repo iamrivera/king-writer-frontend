a2d36214312a3e480e4f00ad4c28c2e31529bf25
iamrivera <a3m3rivera@gmail.com>
2021-09-27 02:57:22 -0700

    Finished Styling the Home Page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div class>
-      <iframe width="1280" height="720" src="https://www.youtube.com/embed/qg0_FinB6EE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div>
+      <h4 className="mt-5 mb-4">
+        Welcome to Monarch Writer 👑 A place for anyone and everyone to find
+        inspiration and 📝 stories. Here's a few words of advice from
+        one of the Monarchs of Literature, Niel Gaiman!
+      </h4>
+      <Container className="mt-5 text-primary bg-dark" fluid="m">
+        <iframe
+          width="1280"
+          height="720"
+          src="https://www.youtube.com/embed/qg0_FinB6EE"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Container>
     </div>
   );
 };
