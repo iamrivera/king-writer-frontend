ed6b7aba5820f4fc0813373ff02168a41d542cbd
iamrivera <a3m3rivera@gmail.com>
2021-09-27 02:16:30 -0700

    Created Quotes full functionality' git push

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
 
 const QuoteList = (props) => {
-  return <div>Hello World</div>;
+  return (
+    <div className="container-fluid mt-4">
+      <Row xs={1} md={2} className="g-4">
+        {props.quotes.map((quote, idx) => (
+          <Col>
+            <Card key={idx}>
+              {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+              <Card.Body>
+                <Card.Title>A Few Words For Thought</Card.Title>
+                <Card.Text>
+                  {quote.content}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default QuoteList;
